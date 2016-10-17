18f7904657091d4a3e610400c23ef7b8a98d0279
Alsu <alsu.rakhmanova@gmail.com>
2016-10-17 15:19:51 -0400

    Added variable x, test if ...

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -17,6 +17,12 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         // TODO code application logic here
       System.out.println("Hello World");
+      
+      int x= 5;
+      if (x > 4)
+      {
+        System.out.println("greater");  
+      }
     }
     
 }
